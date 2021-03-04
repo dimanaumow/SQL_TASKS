1fbafe41908ed414d03ca4599912cac322eb1b79
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2021-03-05 02:19:53 +0300

    Add HAVING

---
+++ HAVING.sql
@@ -0,0 +1,37 @@
+USE [sql-ex]
+GO
+
+
+--Exercise: 15 (Serge I: 2003-02-03)
+--Get hard drive capacities that are identical for two or more PCs. Result set: hd.
+SELECT hd 
+FROM PC
+GROUP BY hd
+HAVING COUNT(*) >= 2
+
+
+--Exercise: 20 (Serge I: 2003-02-13)
+--Find the makers producing at least three distinct models of PCs. Result set: maker, number of PC models.
+SELECT maker, COUNT(model) 
+FROM PRODUCT
+WHERE type = 'pc'
+GROUP BY maker
+HAVING COUNT(model) >= 3
+
+
+--Exercise: 40 (Serge I: 2012-04-20)
+--Get the makers who produce only one product type and more than one model. Output: maker, type.
+SELECT maker, MAX(type) 
+FROM Product
+GROUP BY maker
+HAVING COUNT(DISTINCT type) = 1 AND COUNT(model) > 1
+
+
+--Exercise: 85 (Serge I: 2012-03-16)
+--Get makers producing either printers only or personal computers only; in case of PC manufacturers they should produce at least 3 models.
+SELECT maker 
+FROM Product
+WHERE type = 'PC' OR type = 'PRINTER'
+GROUP BY maker
+HAVING COUNT(DISTINCT type) = 1 AND (MIN(type) = 'PRINTER' OR (MIN(type) = 'PC' AND COUNT(model) >= 3))
+
