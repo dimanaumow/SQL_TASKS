872ae05ba92e5e6941178f502369da0c8d58d070
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2021-03-05 01:44:17 +0300

    Add GROUPBY

---
+++ GROUPBY.sql
@@ -0,0 +1,34 @@
+USE [sql-ex]
+GO
+
+--Exercise: 11 (Serge I: 2002-11-02)
+--Find out the average speed of PCs.
+SELECT AVG(speed) FROM dbo.PC
+
+
+--Exercise: 12 (Serge I: 2002-11-02)
+--Find out the average speed of the laptops priced over $1000.
+SELECT AVG(speed) FROM dbo.Laptop
+WHERE price > 1000
+
+--Exercise: 21 (Serge I: 2003-02-13)
+--Find out the maximum PC price for each maker having models in the PC table. Result set: maker, maximum price.
+SELECT Product.maker, MAX(pc.price) FROM Product, PC
+WHERE Product.model = PC.model
+GROUP BY maker
+
+
+--Exercise: 22 (Serge I: 2003-02-13)
+--For each value of PC speed that exceeds 600 MHz, find out the average price of PCs with identical speeds.
+--Result set: speed, average price.
+SELECT speed, AVG(price) 
+FROM PC
+WHERE speed > 600
+GROUP BY speed
+
+
+--Exercise: 53 (Serge I: 2002-11-05)
+--With a precision of two decimal places, determine the average number of guns for the battleship classes.
+SELECT FORMAT(AVG(CAST(numGuns AS float)), '0.00') FROM Classes
+WHERE type = 'bb'
+GROUP BY type
