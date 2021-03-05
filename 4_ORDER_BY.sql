cd25c49490ef14f4867fdc183c933b91e28874ac
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2021-03-06 00:46:33 +0300

    Add ORDER_BY

---
+++ 4_ORDER_BY.sql
@@ -0,0 +1,35 @@
+USE [sql-ex]
+GO 
+
+
+--Exercise: 18 (Serge I: 2003-02-03)
+--Find the makers of the cheapest color printers. Result set: maker, price.
+SELECT maker, price
+FROM Product INNER JOIN Printer
+ON Product.model = Printer.model
+WHERE price = 
+			(SELECT MIN(price) 
+			FROM Printer
+			WHERE color = 'y')
+			AND color = 'y'
+GROUP BY maker, price
+
+
+--Exercise: 90 (Serge I: 2012-05-04)
+--Display all records from the Product table except for three rows 
+--with the smallest model numbers and three ones with the greatest model numbers.
+SELECT *
+FROM dbo.Product
+WHERE model	NOT IN 
+			(
+			SELECT TOP(3) model
+			FROM dbo.Product
+			ORDER BY model ASC
+
+			UNION 
+
+			SELECT TOP(3) model
+			FROM dbo.Product
+			ORDER BY model DESC
+			)
+
