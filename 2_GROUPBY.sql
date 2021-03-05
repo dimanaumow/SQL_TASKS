861d0c825d04ff7fba548011e38d38be604d3b19
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2021-03-06 01:21:08 +0300

    Add tasks 38 and 86

--- 2_GROUPBY.sql
+++ 2_GROUPBY.sql
@@ -30,9 +30,34 @@ WHERE speed > 600
 GROUP BY speed
 
 
+--Exercise: 38 (Serge I: 2003-02-19)
+--Find countries that ever had classes of both battleships (‘bb’) and cruisers (‘bc’).
+SELECT country 
+FROM Classes
+WHERE type = 'bb' 
+INTERSECT 
+SELECT country 
+FROM Classes
+WHERE type = 'bc'
+
+
 --Exercise: 53 (Serge I: 2002-11-05)
 --With a precision of two decimal places, determine the average number of guns for the battleship classes.
 SELECT FORMAT(AVG(CAST(numGuns AS float)), '0.00') AS [Average num guns] 
 FROM Classes
 WHERE type = 'bb'
 GROUP BY type
+
+
+--Exercise: 86 (Serge I: 2012-04-20)
+--For each maker, list the types of products he produces in alphabetic order, using a slash ("/") as a delimiter.
+--Result set: maker, list of product types.
+
+SELECT maker, 
+		CASE COUNT(DISTINCT type)
+		WHEN 3 THEN 'Laptop/PC/Printer'
+		WHEN 2 THEN CONCAT(MIN(type), '/', MAX(type))
+		WHEN 1 THEN MAX(type)
+		END AS types
+FROM Product
+GROUP BY maker
