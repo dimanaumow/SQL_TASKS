bb509e9e3bd8e1448d6e7355819a53c06a1f4220
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2021-03-10 16:52:31 +0300

    Create 6_JOIN.sql

---
+++ 6_JOIN.sql
@@ -0,0 +1,24 @@
+USE [sql-ex]
+GO
+
+
+--Exercise: 6 (Serge I: 2002-10-28)
+--For each maker producing laptops with a hard drive capacity of 10 Gb or
+--higher, find the speed of such laptops. Result set: maker, speed.
+SELECT maker, 
+	   speed
+  FROM Product 
+  JOIN Laptop 
+    ON Product.model = Laptop.model
+ WHERE hd >= 10
+
+
+--Exercise: 9 (Serge I: 2002-11-02)
+--Find the makers of PCs with a processor speed of 450 MHz or more. Result set: maker.
+SELECT DISTINCT maker
+  FROM PC 
+  JOIN Product 
+    ON PC.model = Product.model
+ WHERE PC.speed >= 450
+   AND type = 'pc'
+
